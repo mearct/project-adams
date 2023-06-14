a1ab7b719bb19289f015beb0ab4666bb77f1c087
DESKTOP-GMCRIPK\test <mearct@gmail.com>
2023-06-15 00:33:47 +0800

    Changing the Login and Register Page Layout

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            // $table->string('firstname')->nullable();
+            // $table->string('lastname')->nullable();
+            // $table->string('middlename')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
